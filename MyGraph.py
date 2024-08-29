9bee11f819e71c3096cab2dc95088cc84c4aeba1
Brian Leone <brian_leone1@baylor.edu>
2024-08-29 15:10:22 -0500

    Initial Commit

---
+++ MyGraph.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
+
+#step 1 - create a vitual envionrment - CMD is py -3 -m venv myvenv (name of virtual enviroment) in terminal
+#step 2 Activate your VE - myvenv(name of env)/Scripts/activate 
+#step 3 Install 3rd party library 
+
+#git is an application that allows to track changes in project, can revert back if needed
